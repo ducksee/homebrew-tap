6b09fccc326a90e54b5739de22091735aabe5434
ducksee <duck@local.git>
2026-07-21 23:26:20 +0800

    duckterm-hookd 0.4.0

--- Formula/duckterm-hookd.rb
+++ Formula/duckterm-hookd.rb
@@ -13,31 +13,41 @@
 class DucktermHookd < Formula
   desc "Connect supported coding agents to the DuckTerm mobile app"
   homepage "https://github.com/ducksee/duckterm-hookd-releases"
-  version "0.3.9"
+  version "0.4.0"
   license :cannot_represent # proprietary (see package LICENSE)
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/ducksee/duckterm-hookd-releases/releases/download/v#{version}/duckterm-hookd_darwin-arm64.tar.gz"
-      sha256 "1a6ae2f1fbc1e3ccaa15a9f9b4996bfae8f8b38b3a2b405a9a7a90cba1720805"
+      sha256 "f1c6f6c3a38805a67cfd201aa0a21373cce4ed86855cefea4968b942ecaff25f"
     else
       url "https://github.com/ducksee/duckterm-hookd-releases/releases/download/v#{version}/duckterm-hookd_darwin-amd64.tar.gz"
-      sha256 "47908d74e55d795aaa12024d17a75cf987471e7eb4bee4bd1cd8fd14b78091f1"
+      sha256 "f1f061ab925e37510a2872bbb01da64dcc9d64aa6e330d344405ce02927e8daa"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/ducksee/duckterm-hookd-releases/releases/download/v#{version}/duckterm-hookd_linux-arm64.tar.gz"
-      sha256 "ebce9ae005d2f192d4629d622c73fc3ef0649e50d659c5abf93e15b76cb1c012"
+      sha256 "d1cd8144899a31746cd13c4cce9e308137d5209236fd2eda025a1727baaf518b"
     else
       url "https://github.com/ducksee/duckterm-hookd-releases/releases/download/v#{version}/duckterm-hookd_linux-amd64.tar.gz"
-      sha256 "78e38a27af4c84bf8e19fbc6f21a1231d4ca13b509d27a0b0773787359d58049"
+      sha256 "435d8d3b9fee4e4372c6db3cc4a458ba647118d1594ee49ff7c6a28e40393c1c"
     end
   end
 
   def install
     bin.install "duckterm-hookd"
+    libexec.install "duckterm-hookd-web.tar.gz" if File.exist?("duckterm-hookd-web.tar.gz")
+  end
+
+  def post_install
+    bundled_ui = libexec/"duckterm-hookd-web.tar.gz"
+    return unless bundled_ui.exist?
+
+    system bin/"duckterm-hookd", "ui", "bootstrap", bundled_ui
+  rescue StandardError => e
+    opoo "Bundled Hookd Web UI was not installed: #{e}. Run `duckterm-hookd ui upgrade` to repair it."
   end
 
   def caveats
